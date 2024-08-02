6bd564599ded40a9f36cd0ca89bc2fc4047b552a
Owen Miller <owen9825@gmail.com>
2024-08-02 21:55:32 +1000

    injecting video with wrong dimensions

--- scripts/video_injection.js
+++ scripts/video_injection.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var videoContainer = document.getElementById('video-container');
-    if (videoContainer) {
-      var videoSrc = 'https://fusionparty.space/video/muted_chat_340.webm';
-      var videoHtml = `
-        <video width="100%" autoplay="" loop="" muted="" playsinline="" class="rounded">
-          <source src="${videoSrc}" type="video/mp4">
-          Your browser must not support the video tag.
-        </video>
-      `;
-      videoContainer.innerHTML = videoHtml;
+    var videoContainers = document.getElementsByClassName('video-placeholder');
+    if (videoContainers.length > 0) {
+        for (var i = 0; i < videoContainers.length; i++) {
+            var videoContainer = videoContainers[i];
+            console.log(`Considering ${videoContainer.id}`);
+            var videoSrc;
+            if (videoContainer.id === 'video-big-ideas-container') {
+                videoSrc = 'https://fusionparty.space/video/muted_chat_340.webm';
+            } else if (videoContainer.id === 'video-events') {
+                videoSrc = 'https://fusionparty.space/video/online_highlights_400.webm';
+            } else {
+                console.error("Unrecognized video container");
+                continue; // Skip this container if the ID is unrecognized
+            }
+            var videoHtml = `
+                <video width="100%" autoplay="" loop="" muted="" playsinline="" class="rounded">
+                    <source src="${videoSrc}" type="video/mp4">
+                    Your browser must not support the video tag.
+                </video>
+            `;
+            videoContainer.innerHTML = videoHtml;
+        }
+    } else {
+        console.log('No video containers found');
     }
 });
