6e650a7aa03558b7be3b7932a74f706537d4f0ed
Owen Miller <owen9825@gmail.com>
2024-08-02 22:33:12 +1000

    Almost ready

--- scripts/spreading-cards.js
+++ scripts/spreading-cards.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     var observer = new IntersectionObserver(function(entries, observer) {
         entries.forEach(function(entry) {
             if (entry.isIntersecting) {
-                console.log('Time to spread!');
                 function slideCardsInContainer(container){
                     // Get the children with the class 'spreadable'
                     var spreadableItems = container.getElementsByClassName('spreadable');
@@ -12,14 +11,14 @@ document.addEventListener("DOMContentLoaded", function() {
                     card1.style.transform = "rotate(-18deg)";
                     var card2 = spreadableItems[1];
                     card2.style.top = "15%";
-                    card2.style.left = "15%";
+                    card2.style.left = "22%";
                     card2.style.transform = "rotate(-5deg)"
                     var card3 = spreadableItems[2];
                     card3.style.top = "10%";
-                    card3.style.left = "45%";
+                    card3.style.left = "65%";
                     card3.style.transform = "rotate(10deg)";
                 }
-                slideCardsInContainer(entry.target);
+                setTimeout(slideCardsInContainer(entry.target), 3000);
                 // Optional: Stop observing the element after animating
                 observer.unobserve(entry.target);
             }
