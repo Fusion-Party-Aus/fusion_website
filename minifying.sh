b8152775516d10ee9ac4de4424de1f796c67747e
Owen Miller <owen9825@gmail.com>
2024-08-03 10:29:47 +1000

    Minifying JS

--- minifying.sh
+++ minifying.sh
@@ -22,3 +22,18 @@ do
 done
 
 echo "CSS minification completed."
+for file in scripts/*.js
+do
+    # Get the filename without the path and extension
+    filename=$(basename "$file" .js)
+    # Generate a hash based on the contents of the CSS file
+    hash=$(md5sum "$file" | cut -d' ' -f1 | cut -c1-8)
+
+    # Minify the JS file and save it with the hash and .min.js extension
+    outputFilename="scripts/minified/${filename}.${hash}.min.js"
+    uglifyjs "${file}" --compress --mangle --output "${outputFilename}"
+    echo "Minified $file -> ${outputFilename}"
+    sed -i "s|scripts/minified/${filename}\.[a-z0-9]\+\.min\.js|scripts/minified/${filename}.${hash}.min.js|g" index.html policy/*.html
+    aws s3api put-object --bucket fusion-assets --endpoint-url "${FUSION_ASSETS_ENDPOINT}" --body "${outputFilename}" --key "${outputFilename}"
+done
+
